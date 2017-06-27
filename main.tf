43c5a354f18a8060bc98ac1fbabc96f977ba734e
Igor Rodionov <goruha@gmail.com>
2017-06-27 17:50:18 +0600

    Added initial code

---
+++ main.tf
@@ -0,0 +1,9 @@
+resource "null_resource" "default" {
+  triggers = {
+    id = "${lower(format("%v-%v-%v", var.namespace, var.stage, var.name))}"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
---
+++ outputs.tf
@@ -0,0 +1,3 @@
+output "value" {
+  value = "${null_resource.default.triggers.id}"
+}
