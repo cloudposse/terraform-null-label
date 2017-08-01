0f3926c12f76577653c8d3b545f9c9256d633caf
Sergey Vasilyev <s2504s@gmail.com>
2017-08-01 11:39:07 +0300

    Added outputs: Name, Stage, Namespace

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,16 @@
 output "id" {
   value = "${null_resource.default.triggers.id}"
 }
+
+output "Name" {
+  value = "${null_resource.default.triggers.name}"
+}
+
+output "Namespace" {
+  value = "${null_resource.default.triggers.namespace}"
+}
+
+output "Stage" {
+  value = "${null_resource.default.triggers.stage}"
+}
+
