a9f895c4bf4eb3f22ad6969517b9379269520c94
Sergey Vasilyev <s2504s@gmail.com>
2017-08-03 11:53:22 +0300

    Added outputs: Name, Stage, Namespace (#3)

--- main.tf
+++ main.tf
@@ -1,6 +1,9 @@
 resource "null_resource" "default" {
   triggers = {
     id = "${lower(format("%v-%v-%v", var.namespace, var.stage, var.name))}"
+    name = "${lower(format("%v", var.name))}"
+    namespace = "${lower(format("%v", var.namespace))}"
+    stage = "${lower(format("%v", var.stage))}"
   }
 
   lifecycle {
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,13 @@
 output "id" {
   value = "${null_resource.default.triggers.id}"
 }
+
+output "tags" {
+  value = "${
+    map(
+      "Name", "${null_resource.default.triggers.id}",
+      "Namespace", "${null_resource.default.triggers.namespace}",
+      "Stage", "${null_resource.default.triggers.stage}"
+    )
+  }"
+}
