c633786f80322c7142dffb5626c5c1cff39c9d1c
Igor Rodionov <goruha@gmail.com>
2017-08-09 20:13:51 +0600

    Added module param

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "null_resource" "default" {
   triggers = {
-    id = "${lower(join("-", compact(list(var.namespace, var.stage, var.name, var.module))))}"
+    id = "${lower(join("-", compact(list(var.namespace, var.stage, var.module, var.name))))}"
     name = "${lower(format("%v", var.name))}"
     namespace = "${lower(format("%v", var.namespace))}"
     stage = "${lower(format("%v", var.stage))}"
