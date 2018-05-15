889061151829a6cf41268b61c0f9eb6df7b9d134
Jamie Nelson <jamie@theinstillery.com>
2018-05-15 12:47:42 +0100

    Fixed up the autoscaling group example to be very clear with many comments.

--- examples/autoscalinggroup/main.tf
+++ examples/autoscalinggroup/main.tf
@@ -1,235 +1,9 @@
-###
-### To test this example, just get a subnet-id, the default security group id and region that subnet is in.
-###
 
-variable "aws_region" {
-  default = "eu-west-2"
-}
-
-provider "aws" {
-  region  = "${var.aws_region}"
-  version = "~> 1.17"
-}
-
-variable "vpc_zone_identifier" {
-  description = "A list of subnet IDs to launch resources in"
-  type        = "list"
-}
-
-variable "create_lc" {
-  description = "Whether to create launch configuration"
-  default     = true
-}
-
-variable "create_asg" {
-  description = "Whether to create autoscaling group"
-  default     = true
-}
-
-variable "name" {
-  description = "Creates a unique name beginning with the specified prefix"
-  default     = "coolproject"
-}
-
-variable "lc_name" {
-  description = "Creates a unique name for launch configuration beginning with the specified prefix"
-  default     = "happyhappy"
-}
-
-variable "asg_name" {
-  description = "Creates a unique name for autoscaling group beginning with the specified prefix"
-  default     = "joyjoy"
-}
-
-variable "launch_configuration" {
-  description = "The name of the launch configuration to use (if it is created outside of this module)"
-  default     = ""
-}
-
-variable "instance_type" {
-  description = "The size of instance to launch"
-  default     = "t2.small"
-}
-
-variable "iam_instance_profile" {
-  description = "The IAM instance profile to associate with launched instances"
-  default     = ""
-}
-
-variable "key_name" {
-  description = "The key name that should be used for the instance"
-  default     = ""
-}
-
-variable "security_groups" {
-  description = "A list of security group IDs to assign to the launch configuration"
-  type        = "list"
-}
-
-variable "associate_public_ip_address" {
-  description = "Associate a public ip address with an instance in a VPC"
-  default     = false
-}
-
-variable "user_data" {
-  description = "The user data to provide when launching the instance"
-  default     = " "
-}
-
-variable "enable_monitoring" {
-  description = "Enables/disables detailed monitoring. This is enabled by default."
-  default     = true
-}
-
-variable "ebs_optimized" {
-  description = "If true, the launched EC2 instance will be EBS-optimized"
-  default     = false
-}
-
-variable "root_block_device" {
-  description = "Customize details about the root block device of the instance"
-  type        = "list"
-  default     = []
-}
-
-variable "ebs_block_device" {
-  description = "Additional EBS block devices to attach to the instance"
-  type        = "list"
-  default     = []
-}
-
-variable "ephemeral_block_device" {
-  description = "Customize Ephemeral (also known as 'Instance Store') volumes on the instance"
-  type        = "list"
-  default     = []
-}
-
-variable "spot_price" {
-  description = "The price to use for reserving spot instances"
-  default     = ""
-}
-
-variable "placement_tenancy" {
-  description = "The tenancy of the instance. Valid values are 'default' or 'dedicated'"
-  default     = "default"
-}
-
-# Autoscaling group
-variable "max_size" {
-  description = "The maximum size of the auto scale group"
-  default     = 1
-}
-
-variable "min_size" {
-  description = "The minimum size of the auto scale group"
-  default     = 1
-}
-
-variable "desired_capacity" {
-  description = "The number of Amazon EC2 instances that should be running in the group"
-  default     = 1
-}
-
-variable "default_cooldown" {
-  description = "The amount of time, in seconds, after a scaling activity completes before another scaling activity can start"
-  default     = 300
-}
-
-variable "health_check_grace_period" {
-  description = "Time (in seconds) after instance comes into service before checking health"
-  default     = 300
-}
-
-variable "health_check_type" {
-  description = "Controls how health checking is done. Values are - EC2 and ELB"
-  default     = "EC2"
-}
-
-variable "force_delete" {
-  description = "Allows deleting the autoscaling group without waiting for all instances in the pool to terminate. You can force an autoscaling group to delete even if it's in the process of scaling a resource. Normally, Terraform drains all the instances before deleting the group. This bypasses that behavior and potentially leaves resources dangling"
-  default     = false
-}
-
-variable "load_balancers" {
-  description = "A list of elastic load balancer names to add to the autoscaling group names"
-  default     = []
-}
-
-variable "target_group_arns" {
-  description = "A list of aws_alb_target_group ARNs, for use with Application Load Balancing"
-  default     = []
-}
-
-variable "termination_policies" {
-  description = "A list of policies to decide how the instances in the auto scale group should be terminated. The allowed values are OldestInstance, NewestInstance, OldestLaunchConfiguration, ClosestToNextInstanceHour, Default"
-  type        = "list"
-  default     = ["Default"]
-}
-
-variable "suspended_processes" {
-  description = "A list of processes to suspend for the AutoScaling Group. The allowed values are Launch, Terminate, HealthCheck, ReplaceUnhealthy, AZRebalance, AlarmNotification, ScheduledActions, AddToLoadBalancer. Note that if you suspend either the Launch or Terminate process types, it can prevent your autoscaling group from functioning properly."
-  default     = []
-}
-
-variable "tags" {
-  description = "A list of tag blocks. Each element should have keys named key, value, and propagate_at_launch."
-  default     = []
-}
-
-variable "tags_as_map" {
-  description = "A map of tags and values in the same format as other resources accept.  This will be zipmapped into the non-standard format that the aws_autoscaling_group requires."
-  type        = "map"
-  default     = {}
-}
-
-variable "placement_group" {
-  description = "The name of the placement group into which you'll launch your instances, if any"
-  default     = ""
-}
-
-variable "metrics_granularity" {
-  description = "The granularity to associate with the metrics to collect. The only valid value is 1Minute"
-  default     = "1Minute"
-}
-
-variable "enabled_metrics" {
-  description = "A list of metrics to collect. The allowed values are GroupMinSize, GroupMaxSize, GroupDesiredCapacity, GroupInServiceInstances, GroupPendingInstances, GroupStandbyInstances, GroupTerminatingInstances, GroupTotalInstances"
-  type        = "list"
-
-  default = [
-    "GroupMinSize",
-    "GroupMaxSize",
-    "GroupDesiredCapacity",
-    "GroupInServiceInstances",
-    "GroupPendingInstances",
-    "GroupStandbyInstances",
-    "GroupTerminatingInstances",
-    "GroupTotalInstances",
-  ]
-}
-
-variable "wait_for_capacity_timeout" {
-  description = "A maximum duration that Terraform should wait for ASG instances to be healthy before timing out. (See also Waiting for Capacity below.) Setting this to '0' causes Terraform to skip all Capacity Waiting behavior."
-  default     = "10m"
-}
-
-variable "min_elb_capacity" {
-  description = "Setting this causes Terraform to wait for this number of instances to show up healthy in the ELB only on creation. Updates will not wait on ELB instance number changes"
-  default     = 0
-}
-
-variable "wait_for_elb_capacity" {
-  description = "Setting this will cause Terraform to wait for exactly this number of healthy instances in all attached load balancers on both create and update operations. Takes precedence over min_elb_capacity behavior."
-  default     = false
-}
-
-variable "protect_from_scale_in" {
-  description = "Allows setting instance protection. The autoscaling group will not select instances with this setting for terminination during scale in events."
-  default     = false
-}
-
+################################
+# terraform-null-label example #
+################################
 module "label" {
-  source    = "github.com/cloudposse/terraform-null-label/terraform-terraform-label.git"
+  source    = "../../"
   namespace = "awesomeproject"
   stage     = "production"
   name      = "clusterpluck"
@@ -244,82 +18,100 @@ module "label" {
   }
 }
 
-data "aws_ami" "ubuntu" {
+#######################
+# Launch template     #
+#######################
+resource "aws_launch_template" "this" {
+  # terraform-null-label example used here: Set template name prefix
+  name_prefix                           = "${module.label.id}-"
+  image_id                              = "${data.aws_ami.amazon_linux.id}"
+  instance_type                         = "t2.micro"
+  instance_initiated_shutdown_behavior  = "terminate"
+
+  vpc_security_group_ids                = ["${data.aws_security_group.default.id}"]
+
+  monitoring {
+    enabled                             = false
+  }
+  # terraform-null-label example used here: Set tags on volumes
+  tag_specifications {
+    resource_type                       = "volume"
+    tags                                = "${module.label.tags}"
+  }
+}
+
+######################
+# Autoscaling group  #
+######################
+resource "aws_autoscaling_group" "this" {
+  # terraform-null-label example used here: Set ASG name prefix
+  name_prefix                           = "${module.label.id}-"
+  vpc_zone_identifier                   = ["${data.aws_subnet_ids.all.ids}"]
+  max_size                              = "1"
+  min_size                              = "1"
+  desired_capacity                      = "1"
+
+  launch_template = {
+    id                                  = "${aws_launch_template.this.id}"
+    version                             = "$$Latest"
+  }
+  
+  # terraform-null-label example used here: Set tags on ASG and EC2 Servers
+  tags                                  = ["${module.label.tags_as_list_of_maps}"]
+}
+
+  # terraform-null-label example used here: Output list of tags applied in each format
+output "tags_as_list_of_maps" {
+  value                                 = ["${module.label.tags_as_list_of_maps}"]
+}
+output "tags" {
+  value                                 = ["${module.label.tags}"]
+}
+
+provider "aws" {
+  region = "eu-west-1"
+  version = "~> 1.17"
+
+  # Make it faster by skipping unneeded checks here
+  skip_get_ec2_platforms      = true
+  skip_metadata_api_check     = true
+  skip_region_validation      = true
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+}
+
+##############################################################
+# Data sources to get VPC, subnets and security group details
+##############################################################
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnet_ids" "all" {
+  vpc_id = "${data.aws_vpc.default.id}"
+}
+
+data "aws_security_group" "default" {
+  vpc_id = "${data.aws_vpc.default.id}"
+  name   = "default"
+}
+
+data "aws_ami" "amazon_linux" {
   most_recent = true
 
   filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
+    name = "name"
+
+    values = [
+      "amzn-ami-hvm-*-x86_64-gp2",
+    ]
   }
 
   filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
+    name = "owner-alias"
+
+    values = [
+      "amazon",
+    ]
   }
-
-  owners = ["099720109477"] # Canonical
-}
-
-#######################
-# Launch configuration
-#######################
-resource "aws_launch_configuration" "this" {
-  count = "${var.create_lc}"
-
-  name_prefix                 = "${module.label.id}-"
-  image_id                    = "${data.aws_ami.ubuntu.id}"
-  instance_type               = "${var.instance_type}"
-  iam_instance_profile        = "${var.iam_instance_profile}"
-  key_name                    = "${var.key_name}"
-  security_groups             = ["${var.security_groups}"]
-  associate_public_ip_address = "${var.associate_public_ip_address}"
-  user_data                   = "${var.user_data}"
-  enable_monitoring           = "${var.enable_monitoring}"
-  spot_price                  = "${var.spot_price}"
-  placement_tenancy           = "${var.spot_price == "" ? var.placement_tenancy : ""}"
-  ebs_optimized               = "${var.ebs_optimized}"
-  ebs_block_device            = "${var.ebs_block_device}"
-  ephemeral_block_device      = "${var.ephemeral_block_device}"
-  root_block_device           = "${var.root_block_device}"
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-####################
-# Autoscaling group
-####################
-resource "aws_autoscaling_group" "this" {
-  count = "${var.create_asg}"
-
-  name_prefix          = "${module.label.id}-"
-  launch_configuration = "${var.create_lc ? element(aws_launch_configuration.this.*.name, 0) : var.launch_configuration}"
-  vpc_zone_identifier  = ["${var.vpc_zone_identifier}"]
-  max_size             = "${var.max_size}"
-  min_size             = "${var.min_size}"
-  desired_capacity     = "${var.desired_capacity}"
-
-  load_balancers            = ["${var.load_balancers}"]
-  health_check_grace_period = "${var.health_check_grace_period}"
-  health_check_type         = "${var.health_check_type}"
-
-  min_elb_capacity          = "${var.min_elb_capacity}"
-  wait_for_elb_capacity     = "${var.wait_for_elb_capacity}"
-  target_group_arns         = ["${var.target_group_arns}"]
-  default_cooldown          = "${var.default_cooldown}"
-  force_delete              = "${var.force_delete}"
-  termination_policies      = "${var.termination_policies}"
-  suspended_processes       = "${var.suspended_processes}"
-  placement_group           = "${var.placement_group}"
-  enabled_metrics           = ["${var.enabled_metrics}"]
-  metrics_granularity       = "${var.metrics_granularity}"
-  wait_for_capacity_timeout = "${var.wait_for_capacity_timeout}"
-  protect_from_scale_in     = "${var.protect_from_scale_in}"
-
-  tags = ["${module.label.tags_as_list_of_maps}"]
-}
-
-output "tags" {
-  value = ["${module.label.tags_as_list_of_maps}"]
 }
