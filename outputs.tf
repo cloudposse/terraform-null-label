3b9db60c28063dc5be457230d12d1eef7c05a748
David Begin <david@10etechnology.com>
2018-07-13 11:03:29 -0700

    Simplify module for Rival's use case

--- outputs.tf
+++ outputs.tf
@@ -8,27 +8,17 @@ output "name" {
   description = "Normalized name"
 }
 
-output "namespace" {
-  value       = "${local.namespace}"
-  description = "Normalized namespace"
+output "env" {
+  value       = "${local.env}"
+  description = "Normalized Env"
 }
 
-output "stage" {
-  value       = "${local.stage}"
-  description = "Normalized stage"
-}
-
-output "attributes" {
-  value       = "${local.attributes}"
-  description = "Normalized attributes"
+output "stack" {
+  value       = "${local.stack}"
+  description = "Normalized Stack"
 }
 
 output "tags" {
   value       = "${local.tags}"
   description = "Normalized Tag map"
-}
-
-output "tags_as_list_of_maps" {
-  value       = ["${local.tags_as_list_of_maps}"]
-  description = "Additional tags as a list of maps, which can be used in several AWS resources"
 }
