22ce9e6faa6bf557255ffba9ccafeb45cb50e643
Erik Osterman <erik@cloudposse.com>
2019-05-30 23:56:41 -0700

    lowercase and strip

--- main.tf
+++ main.tf
@@ -3,16 +3,18 @@ locals {
   defaults = {
     label_order = ["namespace", "environment", "stage", "name", "attributes"]
     delimiter   = "-"
+    replacement = ""
   }
 
   # Provided values provided by variables superceed values inherited from the context
 
   enabled             = var.enabled
-  name                = coalesce(var.name, var.context.name)
-  namespace           = coalesce(var.namespace, var.context.namespace)
-  environment         = coalesce(var.environment, var.context.environment)
-  stage               = coalesce(var.stage, var.context.stage)
   regex_replace_chars = coalesce(var.regex_replace_chars, var.context.regex_replace_chars)
+
+  name                = lower(replace(coalesce(var.name, var.context.name), local.regex_replace_chars, local.defaults.replacement))
+  namespace           = lower(replace(coalesce(var.namespace, var.context.namespace), local.regex_replace_chars, local.defaults.replacement))
+  environment         = lower(replace(coalesce(var.environment, var.context.environment), local.regex_replace_chars, local.defaults.replacement))
+  stage               = lower(replace(coalesce(var.stage, var.context.stage), local.regex_replace_chars, local.defaults.replacement))
   delimiter           = coalesce(var.delimiter, var.context.delimiter, local.defaults.delimiter)
   label_order         = length(var.label_order) > 0 ? var.label_order : (length(var.context.label_order) > 0 ? var.context.label_order : local.defaults.label_order)
   additional_tag_map  = merge(var.context.additional_tag_map, var.additional_tag_map)
@@ -23,10 +25,22 @@ locals {
   )
 
   # FIXME: need to filter out empty tags
-  generated_tags = { for l in keys(local.id_context) : title(l) => local.id_context[l] }
+  generated_tags = { for l in keys(local.id_context) : title(l) => local.id_context[l] if length(local.id_context[l]) > 0 }
 
   tags                 = merge(var.context.tags, local.generated_tags, var.tags)
   tags_as_list_of_maps = data.null_data_source.tags_as_list_of_maps.*.outputs
+
+  id_context = {
+    name        = local.name
+    namespace   = local.namespace
+    environment = local.environment
+    stage       = local.stage
+    attributes  = lower(replace(join(local.delimiter, local.attributes), local.regex_replace_chars, local.defaults.replacement))
+  }
+
+  labels = [for l in local.label_order : local.id_context[l]]
+
+  id = lower(join(local.delimiter, local.labels))
 
   # Context of this label to pass to other label modules
   output_context = {
@@ -41,20 +55,7 @@ locals {
     label_order         = local.label_order
     regex_replace_chars = local.regex_replace_chars
     additional_tag_map  = local.additional_tag_map
-
   }
-
-  id_context = {
-    name        = local.name
-    namespace   = local.namespace
-    environment = local.environment
-    stage       = local.stage
-    attributes  = lower(join(local.delimiter, local.attributes))
-  }
-
-  labels = [for l in local.label_order : local.id_context[l]]
-
-  id = lower(join(local.delimiter, local.labels))
 
 }
 
@@ -69,4 +70,3 @@ data "null_data_source" "tags_as_list_of_maps" {
     var.additional_tag_map,
   )
 }
-
