0e5b204174dab74b6235e0005f609175cef6ec8d
Erik Osterman <erik@cloudposse.com>
2019-06-17 19:16:30 -0700

    Fix name tag and add test

--- test/src/examples_complete_test.go
+++ test/src/examples_complete_test.go
@@ -25,27 +25,35 @@ func TestExamplesComplete(t *testing.T) {
 
 	// Run `terraform output` to get the value of an output variable
 	label1 := terraform.OutputMap(t, terraformOptions, "label1")
+	label1Tags := terraform.OutputMap(t, terraformOptions, "label1_tags")
 
 	// Verify we're getting back the outputs we expect
 	assert.Equal(t, "winstonchurchroom-uat-build-fire-water-earth-air", label1["id"])
+	assert.Equal(t, "winstonchurchroom-uat-build-fire-water-earth-air", label1Tags["Name"])
 
 	// Run `terraform output` to get the value of an output variable
 	label2 := terraform.OutputMap(t, terraformOptions, "label2")
+	label2Tags := terraform.OutputMap(t, terraformOptions, "label2_tags")
 
 	// Verify we're getting back the outputs we expect
 	assert.Equal(t, "charlie+uat+test+fire+water+earth+air", label2["id"])
+	assert.Equal(t, "charlie+uat+test+fire+water+earth+air", label2Tags["Name"])
 
 	// Run `terraform output` to get the value of an output variable
 	label3 := terraform.OutputMap(t, terraformOptions, "label3")
+	label3Tags := terraform.OutputMap(t, terraformOptions, "label3_tags")
 
 	// Verify we're getting back the outputs we expect
 	assert.Equal(t, "starfish.uat.release.fire.water.earth.air", label3["id"])
+	assert.Equal(t, "starfish.uat.release.fire.water.earth.air", label3Tags["Name"])
 
 	// Run `terraform output` to get the value of an output variable
 	label4 := terraform.OutputMap(t, terraformOptions, "label4")
+	label4Tags := terraform.OutputMap(t, terraformOptions, "label4_tags")
 
 	// Verify we're getting back the outputs we expect
 	assert.Equal(t, "cloudposse-uat-big-fat-honking-cluster", label4["id"])
+	assert.Equal(t, "cloudposse-uat-big-fat-honking-cluster", label4Tags["Name"])
 
 	// Run `terraform output` to get the value of an output variable
 	label5 := terraform.OutputMap(t, terraformOptions, "label5")
