8c74185f43d4995ce2cc1c8f832ec8afccc31dc3
Frank <639906+syphernl@users.noreply.github.com>
2021-02-03 09:52:15 +0100

    Update exports/context.tf
    Co-authored-by: Nuru <Nuru@users.noreply.github.com>

--- exports/context.tf
+++ exports/context.tf
@@ -20,7 +20,7 @@
 
 module "this" {
   source  = "cloudposse/label/null"
-  version = "0.23.0" # requires Terraform >= 0.13.0
+  version = "0.23.1" # requires Terraform >= 0.13.0
 
   enabled             = var.enabled
   namespace           = var.namespace
